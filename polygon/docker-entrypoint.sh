a174e5ffff898d036df96fa1972ea8e909a6edef
Yorick Downe <yorickdowne@gmail.com>
2023-06-03 00:27:14 +0000

    aria2c use optional for Bor snapshots

--- polygon/docker-entrypoint.sh
+++ polygon/docker-entrypoint.sh
@@ -19,6 +19,23 @@ extract_files() {
     done < ${compiled_files}
 }
 
+wget_files() {
+    extract_dir=$1
+    compiled_files=$2
+    while read -r line; do
+        if [[ "${line}" == checksum* ]]; then
+            continue
+        fi
+        filename="${line}"
+        echo "Extracting ${filename}"
+        if echo "${filename}" | grep -q "bulk"; then
+            wget ${filename} | zstdcat - | tar -xf - -C ${extract_dir}
+        else
+            wget ${filename} | zstdcat - | tar -xf - -C ${extract_dir} --strip-components=3
+        fi
+    done < ${compiled_files}
+}
+
 # allow the container to be started with `--user`
 # If started as root, chown the `--datadir` and run bor as bor
 if [ "$(id -u)" = '0' ]; then
@@ -67,9 +84,13 @@ else
     cd /var/lib/bor/snapshots
     # download compiled incremental snapshot files list
     aria2c -c -x6 -s6 --auto-file-renaming=false --conditional-get=true --allow-overwrite=true https://snapshot-download.polygon.technology/bor-${NETWORK}-incremental-compiled-files.txt
-    # download all incremental files, includes automatic checksum verification per increment
-    aria2c -c -x6 -s6 --auto-file-renaming=false --conditional-get=true --allow-overwrite=true -i bor-${NETWORK}-incremental-compiled-files.txt
-    extract_files /var/lib/bor/data/bor/chaindata bor-${NETWORK}-incremental-compiled-files.txt
+    if [ "${USE_ARIA}" = "true" ]; then
+        # download all incremental files, includes automatic checksum verification per increment
+        aria2c -c -x6 -s6 --auto-file-renaming=false --conditional-get=true --allow-overwrite=true -i bor-${NETWORK}-incremental-compiled-files.txt
+        extract_files /var/lib/bor/data/bor/chaindata bor-${NETWORK}-incremental-compiled-files.txt
+    else
+        wget_files /var/lib/bor/data/bor/chaindata bor-${NETWORK}-incremental-compiled-files.txt
+    fi
     cd "${workdir}"
     touch /var/lib/bor/setupdone
   fi
